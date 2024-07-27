b8a729258d6c15f9b05bfe5f81560db15ac65fbd
Tatiana Shishenina <tatyana020711@gmail.com>
2024-07-27 11:39:47 +0600

    Init App

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => (
+  <div className="d-flex justify-content-center align-items-center spinner">
+    <div className="spinner-border text-primary mt-2" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  </div>
+);
+
+export default Spinner;
